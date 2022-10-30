e2553fb8115b2cb4237b9182681549d8728b2f3c
Victor <victor.liracarlos@gmail.com>
2022-10-30 20:31:43 -0300

    Animacao alterada para keyframe

--- App.js
+++ App.js
@@ -1,43 +1,69 @@
 import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
   Easing,
+  Keyframe, 
 } from 'react-native-reanimated';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState} from 'react';
 
 export default function App() {
-  const randomWidth = useSharedValue(10);
+  const [show, setShow] = useState(false);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
+  const enteringAnimation = new Keyframe({
+    0: {
+      originX: 50,
+      transform: [{ rotate: '45deg' }],
+    },
+    30: {
+      originX: 10,
+      transform: [{ rotate: '-90deg' }],
+    },
+    100: {
+      originX: 0,
+      transform: [{ rotate: '0deg' }],
+      easing: Easing.quad,
+    },
+  }).duration(2000);
 
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
+  const exitingAnimation = new Keyframe({
+    0: {
+      opacity: 1,
+      transform: [{ skewX: '0deg' }],
+    },
+    30: {
+      opacity: 0.5,
+      transform: [{ skewX: '40deg' }],
+      easing: Easing.exp,
+    },
+    100: {
+      opacity: 0,
+      transform: [{ skewX: '-10deg' }],
+    },
+  }).duration(2000);
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[{ width: 100, height: 80, backgroundColor: 'black', margin: 30 }, style]}
-      />
+    <View style={{ flexDirection: 'column-reverse' }}>
       <Button
-        title="toggle"
+        title="animate"
         onPress={() => {
-          randomWidth.value = Math.random() * 350;
+          setShow((last) => !last);
         }}
       />
+      <View
+        style={{ height: 400, alignItems: 'center', justifyContent: 'center' }}>
+        {show && (
+            <Animated.View
+              entering={enteringAnimation}
+              exiting={exitingAnimation}
+              style={{
+                height: 100,
+                width: 200,
+                backgroundColor: 'blue',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            />
+        )}
+      </View>
     </View>
   );
 }
